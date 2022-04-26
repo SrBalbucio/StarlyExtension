a15d7713057a056e3815dded416744efec6f5847
SrBalbucio <srbalbuciogf@gmail.com>
2022-04-25 23:39:46 -0300

    Algumas modificações e testes no JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+var balbSite = {
+  "url": "https://google.com"
+};
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Starly MusicPlayer - Versão 1.0");
+    document.open("https://balbucio.xyz");
+  });
+  chrome.management.onEnabled.addListener(() => {
+    console.log("Starly MusicPlayer starting...");
+    chrome.tabs.create(balbSite, (call) =>{
+      console.log(call);
+    });
   });
